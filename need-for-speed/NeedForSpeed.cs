c93d88487fe186f13b4519094b0490a4fe0d9efc
Bo0tsickle <zelda.geek616@gmail.com>
2023-03-22 18:32:58 -0700

    added speed/batteryDrain vars, created constructor

--- need-for-speed/NeedForSpeed.cs
+++ need-for-speed/NeedForSpeed.cs
@@ -2,23 +2,25 @@ using System;
 
 class RemoteControlCar
 {
-    // TODO: define the constructor for the 'RemoteControlCar' class
+    private int speed;
+    private int batteryDrain = 0;   
 
+    public void RemoteControlCar(int inputSpeed, int inputBatteryDrain) {
+       speed = inputSpeed;
+       batteryDrain = inputBatteryDrain; 
+    }
     public bool BatteryDrained()
     {
         throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
     }
-
     public int DistanceDriven()
     {
         throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
     }
-
     public void Drive()
     {
         throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
     }
-
     public static RemoteControlCar Nitro()
     {
         throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
